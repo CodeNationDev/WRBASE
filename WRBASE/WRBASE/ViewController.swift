efd7915598354f8361438d92f176189dd5edb181
David Martin Saiz <davms81@gmail.com>
2020-11-19 13:09:56 +0100

    Initial commit

---
+++ WRBASE/WRBASE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WRBASE
+//
+//  Created by david.martin.saiz on 19/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
