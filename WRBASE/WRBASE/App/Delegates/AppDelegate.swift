fa1de146425c3f07ab52f0d339bc2b2edb575137
David Martin Saiz <davms81@gmail.com>
2020-11-26 23:19:23 +0100

    Change CoreData model

--- WRBASE/WRBASE/App/Delegates/AppDelegate.swift
+++ WRBASE/WRBASE/App/Delegates/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Logger")
+        let container = NSPersistentContainer(name: "WRBASE")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
        
